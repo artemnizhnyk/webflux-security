412867c02a690b6fc1175de4f8ee56c6d030132b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-25 19:32:44 +0200

    dependencies were added

--- src/main/java/com/artemnizhnyk/webfluxsecurity/WebfluxSecurityApplication.java
+++ src/main/java/com/artemnizhnyk/webfluxsecurity/WebfluxSecurityApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebfluxSecurityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebfluxSecurityApplication.class, args);
     }
-
 }
