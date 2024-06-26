9b1295e7b82f4918dbf0f558b3b2c9c1c9cab6e8
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-26 17:58:32 +0200

    WebSecurityConfig was added

---
+++ src/main/java/com/artemnizhnyk/webfluxsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.artemnizhnyk.webfluxsecurity.config;
+
+import com.artemnizhnyk.webfluxsecurity.security.AuthenticationManager;
+import com.artemnizhnyk.webfluxsecurity.security.BearerTokenServerAuthenticationConverter;
+import com.artemnizhnyk.webfluxsecurity.security.JwtHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@EnableReactiveMethodSecurity
+@Configuration
+public class WebSecurityConfig {
+    @Value("${jwt.secret}")
+    private String secret;
+    private final String[] publicRouts = {"/api/v1/auth/register", "/api/v1/auth/login"};
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, AuthenticationManager authenticationManager) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> {
+                    authorizeExchangeSpec.anyExchange().authenticated();
+                    authorizeExchangeSpec.pathMatchers(HttpMethod.OPTIONS).permitAll();
+                    authorizeExchangeSpec.pathMatchers(publicRouts).permitAll();
+                })
+                .addFilterAt(bearerAuthenticationFilter(authenticationManager), SecurityWebFiltersOrder.AUTHENTICATION)
+
+                .exceptionHandling(exceptionHandlingSpec -> {
+                    exceptionHandlingSpec.authenticationEntryPoint((swe, e) -> {
+                        log.error("In securityWebFilterChain - unauthorized error: {}", e.getMessage());
+                        return Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED));
+                    });
+                    exceptionHandlingSpec.accessDeniedHandler((swe, e) -> {
+                        log.error("In securityWebFilterChain - access denied: {}", e.getMessage());
+                        return Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN));
+                    });
+                })
+                .build();
+    }
+
+    private AuthenticationWebFilter bearerAuthenticationFilter(AuthenticationManager authenticationManager) {
+        AuthenticationWebFilter bearerAuthenticationFilter = new AuthenticationWebFilter(authenticationManager);
+        bearerAuthenticationFilter.setServerAuthenticationConverter(
+                new BearerTokenServerAuthenticationConverter(
+                        new JwtHandler(secret)));
+        bearerAuthenticationFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/**"));
+
+        return bearerAuthenticationFilter;
+    }
+}
