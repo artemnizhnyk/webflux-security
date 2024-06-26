986c49a2df9cf7158b048a6b7d5961180829180c
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-26 17:30:26 +0200

    AuthenticationManager was added

---
+++ src/main/java/com/artemnizhnyk/webfluxsecurity/security/AuthenticationManager.java
@@ -0,0 +1,25 @@
+package com.artemnizhnyk.webfluxsecurity.security;
+
+import com.artemnizhnyk.webfluxsecurity.entity.UserEntity;
+import com.artemnizhnyk.webfluxsecurity.exception.UnauthorizedException;
+import com.artemnizhnyk.webfluxsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class AuthenticationManager implements ReactiveAuthenticationManager {
+    private final UserRepository userRepository;
+
+    @Override
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
+        return userRepository.findById(principal.getId())
+                .filter(UserEntity::isEnabled)
+                .switchIfEmpty(Mono.error(new UnauthorizedException("User disabled")))
+                .map(user -> authentication);
+    }
+}
